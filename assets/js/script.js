c9567dadba113d4c3b65ddeb927acc9b3bd51292
ConnieMarie <constance.marie.strahan@gmail.com>
2022-08-15 16:10:03 -0500

    troubleshooting timetracker function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,38 @@
 // load everything in document before running functions
-$(document).ready(function() {
-//  Display current date
-var todayDate = moment().format('MMMM Do YYYY, h:mm a');
-$("#currentDay").html(todayDate) 
+// $(document).ready(function() {
+//  Display current date/day/time
+var todayDate = moment().format('LLLL');
+$("#currentDay").html(todayDate); 
+
+
+// console.log(todayDate);
  
 //click event listener added to save button
-$(".saveBtn").on("Click", function(event) {
+$(".saveBtn").on("click", function() {
+    // console.log(this);
 // variables for time and description
-var text = $(this).sibling("description").val();
+var text = $(this).siblings(".description").val();
 var time = $(this).parent().attr("id");
+// console.log(text, time);
+
     
 //store saved content in local storage
 localStorage.setItem(text, time);
-   });
+   })
 
    
+   // check current hour block
+function timeTracker() {
+    var hourNow = moment().hour();
+    console.log(hourNow);
+    //console logs current hour only when not wrapped in funtion ??
+    }
 
-})
+
+    // load items from local storage to each time block 
+    
+// })
+
+
 
 
