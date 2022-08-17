051560e82e44f69a9249c4104e01912fa01398f3
ConnieMarie <constance.marie.strahan@gmail.com>
2022-08-17 13:10:18 -0500

    final cleaned up product with console logs removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,6 @@ $(".saveBtn").on("click", function() {
 // variables for time and description
 var text = $(this).siblings(".description").val();
 var time = $(this).parent().attr("id");
-
-
     
 //store saved content in local storage
 localStorage.setItem(time, text);
@@ -26,34 +24,27 @@ function timeTracker() {
     for (let i = 0; i < blockHour.length; i++) {
     
     // load items from local storage to each time block 
-    $(textBlock[i]).val(localStorage.getItem($(textBlock[i]).parent().attr("id")))
-    console.log($(textBlock[i]).parent().parent().attr("id"))
+    $(textBlock[i]).val(localStorage.getItem($(textBlock[i]).parent().attr("id")));
     
     // determine if time block is in the past, present, or future/assign classes accordingly
     if (hourNow > hourBlock) {
         $(blockHour[i]).removeClass("present");
         $(blockHour[i]). removeClass("future");
         $(blockHour[i]).addClass("past");
-        console.log("past");
-        console.log(blockHour[i])
     }
     else if (hourNow === hourBlock) {
         $(blockHour[i]).removeClass("future");
         $(blockHour[i]).removeClass("past");                  
-        $(blockHour[i]).addClass("present");
-        console.log("present");
+        $(blockHour[i]).addClass("present");   
     }
     else {
         $(blockHour[i]).removeClass("past");
         $(blockHour[i]).removeClass("present");
-        $(blockHour[i]).addClass("future");
-        console.log("future");
+        $(blockHour[i]).addClass("future"); 
     }
     hourBlock++
 }
 };
-
-// }
 // call timeTracker function    
 timeTracker();
 
